f9ac24d27fa0133e883977ca8ac8a172cc09dace
Zoltan <nadalzoltan@icloud.com>
2024-12-06 21:58:59 +0100

    Vložené TourPurchase, Traveler

--- viewer/admin.py
+++ viewer/admin.py
@@ -7,7 +7,13 @@ admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Hotel)
 admin.site.register(Airport)
-admin.site.register(TravelInfo)
+
+@admin.register(TravelInfo)
+class TravelInfoAdmin(admin.ModelAdmin):
+    list_display = ("tour_name", "departure_city", "destination_city", "departure_date", "return_date", "price_per_adult", "price_per_child")
+    search_fields = ("tour_name", "departure_city__name", "destination_city__name")
+    list_filter = ("departure_date", "return_date", "meal_type", "is_promoted")
+
 @admin.register(TourPurchase)
 class TourPurchaseAdmin(admin.ModelAdmin):
     list_display = ("travel_info", "adult_count", "child_count", "total_quantity", "total_price", "created_at")
