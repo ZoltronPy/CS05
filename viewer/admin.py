f2634d1a849ec8963cdc309c15ed8aebd361ac44
Zoltan <nadalzoltan@icloud.com>
2024-12-06 21:49:58 +0100

    Vložené TourPurchase, Traveler

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from viewer.models import Continent, Country, City, Hotel, Airport, TravelInfo
+from viewer.models import Continent, Country, City, Hotel, Airport, TravelInfo, TourPurchase
 
 admin.site.register(Continent)
 admin.site.register(Country)
@@ -8,3 +8,8 @@ admin.site.register(City)
 admin.site.register(Hotel)
 admin.site.register(Airport)
 admin.site.register(TravelInfo)
+@admin.register(TourPurchase)
+class TourPurchaseAdmin(admin.ModelAdmin):
+    list_display = ("travel_info", "adult_count", "child_count", "total_quantity", "total_price", "created_at")
+    search_fields = ("travel_info__tour_name",)
+    list_filter = ("created_at",)
