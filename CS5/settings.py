ec93a842676e400380a1ab5aee1781325b401924
Zoltan <nadalzoltan@icloud.com>
2024-12-07 23:30:10 +0100

    Barevné téma , hra s CSS

--- CS5/settings.py
+++ CS5/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+from django.views import static
 from dotenv import load_dotenv
 
 import viewer
@@ -111,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
