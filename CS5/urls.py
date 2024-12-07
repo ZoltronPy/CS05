d258f1c014f2924e052affe87b853236001da9b0
Zoltan <nadalzoltan@icloud.com>
2024-12-07 20:50:46 +0100

    funkční web, tlačítko Home a All trips list .

--- CS5/urls.py
+++ CS5/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from viewer.admin import custom_admin_site
-from viewer.views import all_trips
+from viewer.views import all_trips, home
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls),
 
-    path('trips/', all_trips)
+    path('', home ,name='home'),
+    path('trips/', all_trips, name='all_trips'),
 
 
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from viewer.models import TravelInfo
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def all_trips(request):
     trips = TravelInfo.objects.all()  # Předpokládáme, že načítáš všechny zájezdy
     context = {
