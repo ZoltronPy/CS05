717e09441ee1059f375493a90632ba493426e0d0
Zoltan <nadalzoltan@icloud.com>
2024-12-07 22:16:29 +0100

    home,-triplist-connection,-page-,-payment

--- CS5/urls.py
+++ CS5/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from viewer import views
 from viewer.admin import custom_admin_site
-from viewer.views import all_trips, home
 
 urlpatterns = [
-    path('admin/', custom_admin_site.urls),
-
-    path('', home ,name='home'),
-    path('trips/', all_trips, name='all_trips'),
-
-
+    path('admin/', custom_admin_site.urls),  # Vlastní administrátorský panel
+    path('', views.homepage, name='homepage'),  # Domovská stránka
+    path('trips/', views.trip_list, name='trip_list'),  # Seznam zájezdů
+    path('trips/<int:trip_id>/', views.trip_detail, name='trip_detail'),  # Detail zájezdu
+    path('purchase/', views.purchase_trip, name='purchase_trip'),  # Nákup zájezdu
 ]
