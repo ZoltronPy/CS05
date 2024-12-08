2ec1d8ce6e91b672951f4b1601f6ac671cb92a08
Zoltan <nadalzoltan@icloud.com>
2024-12-08 19:03:50 +0100

    Barevné téma , hra s CSS, opravený purchase form.

--- viewer/admin.py
+++ viewer/admin.py
@@ -3,26 +3,53 @@ from django.contrib.admin import AdminSite
 from viewer.models import Continent, Country, City, Hotel, Airport, TravelInfo, TourPurchase
 
 
+# Inline pro státy
+class CountryInline(admin.TabularInline):
+    model = Country
+    extra = 1  # Počet prázdných formulářů pro přidání nového státu
+
+
+# Inline pro města
+class CityInline(admin.TabularInline):
+    model = City
+    extra = 1  # Počet prázdných formulářů pro přidání nového města
+
+
+class HotelInline(admin.TabularInline):
+    model = Hotel  # Definujeme, že inline se týká modelu Hotel
+    extra = 1  # Počet prázdných formulářů pro přidání hotelu
+
+
+# Custom Admin pro Continent
 class ContinentAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    inlines = [CountryInline]  # Přidá inline pro státy při editaci kontinentu
 
 
+# Custom Admin pro Country
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name', 'continent')
+    inlines = [CityInline]  # Přidá inline pro města při editaci státu
 
 
+# Admin pro City
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'Country')
+    list_display = ('name', 'country',)
+    inlines = [HotelInline]
 
 
+# Admin pro Hotel
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('name', 'City')
+    search_fields = ('name', 'City__name')
 
 
+# Admin pro Airport
 class AirportAdmin(admin.ModelAdmin):
     list_display = ('name', 'City')
 
 
+# Admin pro TravelInfo
 class TravelInfoAdmin(admin.ModelAdmin):
     list_display = (
         "tour_name", "departure_city", "destination_city",
@@ -33,28 +60,87 @@ class TravelInfoAdmin(admin.ModelAdmin):
     list_filter = ("departure_date", "return_date", "meal_type", "is_promoted")
 
 
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+from viewer.models import Continent, Country, City, Hotel, Airport, TravelInfo, TourPurchase
+
+# Inline pro státy
+class CountryInline(admin.TabularInline):
+    model = Country
+    extra = 1  # Počet prázdných formulářů pro přidání nového státu
+
+# Inline pro města
+class CityInline(admin.TabularInline):
+    model = City
+    extra = 1  # Počet prázdných formulářů pro přidání nového města
+
+class HotelInline(admin.TabularInline):
+    model = Hotel  # Definujeme, že inline se týká modelu Hotel
+    extra = 1  # Počet prázdných formulářů pro přidání hotelu
+
+# Custom Admin pro Continent
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [CountryInline]  # Přidá inline pro státy při editaci kontinentu
+
+# Custom Admin pro Country
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'continent')
+    inlines = [CityInline]  # Přidá inline pro města při editaci státu
+
+# Admin pro City
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country',)
+    inlines = [HotelInline]
+
+# Admin pro Hotel
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'City')
+
+    # Ensure City is always displayed properly
+    def City(self, obj):
+        return obj.City.name if obj.City else 'No City Assigned'
+
+    search_fields = ('name', 'City__name')
+
+# Admin pro Airport
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'City')
+
+# Admin pro TravelInfo
+class TravelInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        "tour_name", "departure_city", "destination_city",
+        "formatted_departure_date", "formatted_return_date",
+        "price_per_adult", "price_per_child", "formatted_created_at", "formatted_updated_at",
+    )
+    search_fields = ("tour_name", "departure_city__name", "destination_city__name")
+    list_filter = ("departure_date", "return_date", "meal_type", "is_promoted")
+
+# Admin pro TourPurchase
 class TourPurchaseAdmin(admin.ModelAdmin):
     list_display = (
-        'get_tour_name', 'adult_count', 'child_count', 'total_quantity', 'total_price', 'formatted_created_at', 'formatted_updated_at'
+        'get_tour_name', 'adult_count', 'child_count', 'total_quantity', 'total_price', 'formatted_created_at',
+        'formatted_updated_at'
     )
     search_fields = ('travel_info__tour_name',)
     list_filter = ('travel_info__tour_name',)
 
     def get_tour_name(self, obj):
         return obj.travel_info.tour_name if obj.travel_info else 'No Tour Name Available'
+
     get_tour_name.short_description = 'Tour Name'
 
-
+# Vlastní Admin Site
 class CustomAdminSite(AdminSite):
-    site_header = "Admin - Costumed "
+    site_header = "Admin - Custom"
     site_title = "Admin Panel"
     index_title = "Welcome to the Admin Panel"
 
-
 # Vytvoření instance pro nový Admin Site
-custom_admin_site = CustomAdminSite(name='Admin SITE ')
+custom_admin_site = CustomAdminSite(name='Admin SITE')
 
-# Registrace všech admin modelů
+# Registrace adminů pro modely
 custom_admin_site.register(Continent, ContinentAdmin)
 custom_admin_site.register(Country, CountryAdmin)
 custom_admin_site.register(City, CityAdmin)
--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.utils.text import slugify
 from django.utils.timezone import now
 
 import logging
@@ -12,7 +13,10 @@ from django.db.models import Model, CharField, ForeignKey, CASCADE, TextField, I
 
 #'vytvoření kontinetu'
 class Continent(Model):
-    name = CharField(max_length=32, unique=True, null=False, blank=False, db_index=True)
+    name = CharField(max_length=32,
+                     unique=True, null=False,
+                     blank=False, db_index=True,
+                     verbose_name="NAME")
 
     class Meta:
         ordering = ['name']
@@ -21,7 +25,7 @@ class Continent(Model):
         return f'Continent(name={self.name})'
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 
 #vytvoření Států
@@ -36,48 +40,58 @@ class Country(Model):
         verbose_name_plural = 'Countries'
 
     def __repr__(self):
-        return f'Country(name={self.name})'
+        return f'Country(name={self.name}, continent={self.continent.name})'
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.continent.name})"
 
 
 # Město
 class City(Model):
     name = CharField(max_length=32, null=False, blank=False, unique=True)
-    Country = ForeignKey(Country, on_delete=CASCADE, null=False, blank=False,
+    country = ForeignKey(Country, on_delete=CASCADE, null=False, blank=False,
                          related_name="cities")
     description = TextField(null=True, blank=True)
 
-    hotels = models.ManyToManyField('viewer.Hotel', related_name='cities_set', blank=True)
+    @property
+    def continent_code(self):
+        if self.country and self.country.continent:
+            return self.country.continent.name
 
     class Meta:
         ordering = ['name']
         verbose_name_plural = 'Cities'
 
     def __repr__(self):
-        return f'City(name={self.name})'
+        return (f'City(name={self.name},'
+                f' country={self.country.name}, continent={self.continent_code})')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.country.name}"
 
 
 class Hotel(Model):
     name = CharField(max_length=32, null=False, blank=False, unique=True)
-    Stars = IntegerField(null=True, blank=True, default=0)
+    Stars = IntegerField(null=True, blank=True, default=3)
     Description = TextField(null=True, blank=True)
 
-    City = ForeignKey(City, on_delete=CASCADE, related_name="city_hotels", blank=True, null=True)
+    City = ForeignKey(City, on_delete=CASCADE, related_name="city_hotels",
+                      null=False, blank=False, )
+
+    def clean(self):
+        if not self.City:
+            raise ValidationError("Hotel musí mít přiřazené město.")
 
     class Meta:
         ordering = ['name']
         verbose_name_plural = 'Hotels'
 
     def __repr__(self):
-        return f'Hotel(name={self.name})'
+        return (f'Hotel(name={self.name},'
+                f' {self.City.name if self.City else "No City Assigned"})')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.City.name if self.City else 'No City Assigned'}"
 
 
 class Airport(Model):
@@ -89,36 +103,131 @@ class Airport(Model):
         verbose_name_plural = 'Airports'
 
     def __repr__(self):
-        return f'Airport(name={self.name})'
+        return f'Airport(name={self.name}, city={self.City.name})'
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.City.name}"
 
 
 class TravelInfo(Model):
     #'Departure and destination (city, airport, hotel'
 
-    tour_name = CharField(max_length=32, null=False, blank=False, unique=True, default='Default Tour Name')
+    tour_name = CharField(max_length=32, null=False,
+                          blank=False,
+                          unique=True,
+                          default='Tour name ')
+    city = ForeignKey(City, on_delete=CASCADE,
+                      related_name="city_travel_info")
+    Hotel = ForeignKey(Hotel, on_delete=CASCADE,
+                       related_name="hotels")
 
-    departure_city = ForeignKey(City, on_delete=CASCADE, related_name="departures", null=True, blank=True,
+    departure_city = ForeignKey(City, on_delete=CASCADE,
+                                related_name="departures",
+                                null=True,
+                                blank=True,
                                 verbose_name="Departure (city)")
-    destination_hotels = models.ManyToManyField(Hotel, related_name='travel_infos', blank=True)
 
-    departure_airport = ForeignKey(Airport, on_delete=CASCADE, related_name="departures_airport", null=True, blank=True,
+    departure_airport = ForeignKey(Airport, on_delete=CASCADE,
+                                   related_name="departures_airport",
+                                   null=True, blank=True,
                                    verbose_name="Departure (airport)")
-    destination_city = ForeignKey(City, on_delete=CASCADE, related_name="arrivals", null=True, blank=True,
+
+    destination_city = ForeignKey(City, on_delete=CASCADE,
+                                  related_name="arrivals",
+                                  null=True,
+                                  blank=True,
                                   verbose_name="Destination (city)")
-    destination_airport = ForeignKey(Airport, on_delete=CASCADE, related_name="arrivals_airport", null=True, blank=True,
+
+    destination_airport = ForeignKey(Airport, on_delete=CASCADE,
+                                     related_name="arrivals_airport",
+                                     null=True,
+                                     blank=True,
                                      verbose_name="Destination (airport)")
+
     description = models.TextField(null=True, blank=True)
 
     # Time details
-    departure_date = DateField(verbose_name="Departure date", null=True, blank=True)
-    return_date = DateField(verbose_name="Return date", null=True, blank=True)
-    stay_duration = IntegerField(verbose_name="Duration of stay (days)", null=True, blank=True)
+    departure_date = DateField(verbose_name="Departure date",
 
-    created_at = models.DateTimeField(default=timezone.now, verbose_name="Created At")
-    updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name="Updated At")
+                               null=True,
+                               blank=True)
+    return_date = DateField(verbose_name="Return date",
+                            null=True,
+                            blank=True)
+
+    stay_duration = IntegerField(verbose_name="Duration of stay (days)",
+                                 null=True,
+                                 blank=True)
+
+    created_at = models.DateTimeField(default=timezone.now,
+                                      verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, null=True,
+                                      verbose_name="Updated At")
+
+    # Meal type
+    MEAL_TYPE_CHOICES = [
+        ('BB', 'Bed & Breakfast'),
+        ('HB', 'Half Board'),
+        ('FB', 'Full Board'),
+        ('AI', 'All Inclusive'),
+    ]
+    meal_type = CharField(
+        max_length=2,
+        choices=MEAL_TYPE_CHOICES,
+        verbose_name="Meal type",
+    )
+
+    # Promotion
+    is_promoted = models.BooleanField(default=False, verbose_name="Is promoted")
+
+    # Number of seats
+    adult_seats = IntegerField(verbose_name="Number of seats")
+    child_seats = IntegerField(verbose_name="Number of seats for Kids")
+
+    # Pricing
+    price_per_adult = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Price per passenger (czk)"
+    )
+    price_per_child = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Price per Kid (czk)"
+    )
+
+    class Meta:
+        verbose_name = "Travel Information"
+        verbose_name_plural = "Travel Information"
+        ordering = ["tour_name"]
+
+    def __str__(self):
+        departure_city = self.departure_city.name if self.departure_city else "Unknown Departure City"
+        destination_city = self.destination_city.name if self.destination_city else "Unknown Destination City"
+        return f"Trip: {self.tour_name} | Departure: {departure_city} -> {destination_city}"
+
+    def clean(self):
+        if self.departure_date and self.return_date and self.departure_date > self.return_date:
+            raise ValidationError("Departure date cannot be later than return date.")
+
+    def save(self, *args, **kwargs):
+        # Automatically set the city based on hotel selection
+        if self.Hotel:
+            self.city = self.Hotel.City  # Assuming Hotel model has a 'city' field
+
+            # Set the destination city to be the same as the hotel city, if not already set
+            if not self.destination_city:
+                self.destination_city = self.city
+
+            # Automatically set the departure city to the same as the destination city, if not provided
+            if not self.departure_city:
+                self.departure_city = self.city
+
+        # Automatically generate the tour_name if not set
+        if not self.tour_name:
+            self.tour_name = f"Tour to {self.city.name} - {self.Hotel.name}"
+
+        super().save(*args, **kwargs)
 
     def formatted_departure_date(self):
         if self.departure_date:
@@ -145,47 +254,8 @@ class TravelInfo(Model):
     formatted_created_at.short_description = 'Created At'
     formatted_updated_at.short_description = 'Updated At'
 
-    # Meal type
-    MEAL_TYPE_CHOICES = [
-        ('BB', 'Bed & Breakfast'),
-        ('HB', 'Half Board'),
-        ('FB', 'Full Board'),
-        ('AI', 'All Inclusive'),
-    ]
-    meal_type = CharField(
-        max_length=2,
-        choices=MEAL_TYPE_CHOICES,
-        verbose_name="Meal type",
-    )
 
-    # Pricing
-    price_per_adult = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="Price per passenger (czk)"
-    )
-    price_per_child = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="Price per Kid (czk)"
-    )
 
-    # Promotion
-    is_promoted = models.BooleanField(default=False, verbose_name="Is promoted")
-
-    # Number of seats
-    adult_seats = IntegerField(verbose_name="Number of seats")
-    child_seats = IntegerField(verbose_name="Number of seats for Kids")
-
-    class Meta:
-        verbose_name = "Travel Information"
-        verbose_name_plural = "Travel Information"
-        ordering = ["tour_name"]
-
-    def __str__(self):
-        departure_city = self.departure_city.name if self.departure_city else "Unknown Departure City"
-        destination_city = self.destination_city.name if self.destination_city else "Unknown Destination City"
-        return f"Trip: {self.tour_name} | Departure: {departure_city} -> {destination_city}"
-
-    def clean(self):
-        if self.departure_date and self.return_date and self.departure_date > self.return_date:
-            raise ValidationError("Departure date cannot be later than return date.")
 
 
 class TourPurchase(Model):
@@ -211,15 +281,18 @@ class TourPurchase(Model):
     # Metoda pro získání názvu zájezdu
     def get_tour_name(self):
         return self.travel_info.tour_name if self.travel_info else "No Tour Name"
+
     get_tour_name.short_description = "Tour Name"
 
     # Formátované datumy pro administraci
     def formatted_created_at(self):
         return self.created_at.strftime('%d.%m.%Y') if self.created_at else '-'
+
     formatted_created_at.short_description = 'Created At'
 
     def formatted_updated_at(self):
         return self.updated_at.strftime('%d.%m.%Y') if self.updated_at else '-'
+
     formatted_updated_at.short_description = 'Updated At'
 
     # Reprezentace objektu
@@ -247,3 +320,14 @@ class TourPurchase(Model):
         verbose_name = "Tour Purchase"
         verbose_name_plural = "Tour Purchases"
         ordering = ["-created_at"]
+
+
+class ContinentCountryCity(models.Model):
+    continent_name = models.CharField(max_length=32, unique=True)
+    country_name = models.CharField(max_length=50)
+    international_code = models.CharField(max_length=10, blank=True, null=True)
+    city_name = models.CharField(max_length=32)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.city_name}, {self.country_name}, {self.continent_name}"
